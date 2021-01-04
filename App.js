a64dc8264b8ca7f0c6f591fe9614ac0189f5cd12
Talha Akhter <talhaakhter01@gmail.com>
2021-01-04 10:59:51 -0600

    appload and font

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
 <<<<<<< HEAD
 import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+const fetchFonts = () => {
+  return Font.loadAsync ({
+    'open-sans': require ('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require ('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App () {
   const [userNumber, setUserNumber] = useState ();
 
-  const configureNewGameHandler = () => {
-    setGameRounds(0);
-    setUserNumber(null);
+  const [dataLoaded, setDataLoaded] = useState (false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded (true)}
+        onError={console.warn}
+      />
+    );
   }
+
+  const configureNewGameHandler = () => {
+    setGameRounds (0);
+    setUserNumber (null);
+  };
 
   const startGameHandler = selectedNumber => {
     setUserNumber (selectedNumber);
@@ -30,8 +52,14 @@ export default function App () {
     content = (
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
     );
-  } else if(guessRounds > 0) {
-    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler} />
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
 
   return (
