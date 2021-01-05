410a79089432cddd2ca191e9b3a8a28e839b3278
Talha Akhter <talhaakhter01@gmail.com>
2021-01-05 17:17:34 -0600

    issue

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Colors from '../constants/Colors';
+<<<<<<< HEAD
 import TitleText from './TitleText';
+=======
+>>>>>>> 3633d75... app prototype finalized
 
 const Header = props => {
   return (
     <View style={styles.header}>
+<<<<<<< HEAD
       <TitleText>{props.title}</TitleText>
+=======
+      <Text style={styles.headerTitle}>{props.title}</Text>
+>>>>>>> 3633d75... app prototype finalized
     </View>
   );
 };
@@ -19,6 +26,13 @@ const styles = StyleSheet.create ({
     backgroundColor: Colors.primary,
     alignItems: 'center',
   },
+<<<<<<< HEAD
+=======
+  headerTitle: {
+    color: '#000000',
+    fontSize: 18,
+  },
+>>>>>>> 3633d75... app prototype finalized
 });
 
 export default Header;
