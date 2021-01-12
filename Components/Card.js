ba719538213340607759c9c58cc32db9831aee4f
Talha Akhter <talhaakhter01@gmail.com>
2021-01-12 08:22:23 -0600

    Section 5 finalized

--- Components/Card.js
+++ Components/Card.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 const Card = props => {
   return (
-    <View style={{...styles.card, ...props.style}}>
-      {props.children}
-    </View>
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
   );
 };
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   card: {
-    // Shadow works only on IOS not on Android
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.26,
-    backgroundColor: '#fff',
-    // Shadow for Android
-    elevation: 5,
+    elevation: 8,
+    backgroundColor: 'white',
     padding: 20,
-    borderRadius: 10,
-  },
+    borderRadius: 10
+  }
 });
 
 export default Card;
