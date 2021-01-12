10cfe3b49fd7ddc35cef03fcb2d223d778110bc9
Talha Akhter <talhaakhter01@gmail.com>
2021-01-12 07:48:10 -0600

    fixes

--- components/NumberContainer.js
+++ components/NumberContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import Color from '../constants/Colors';
+import Color from '../constants/colors';
 
 const NumberContainer = props => {
   return (
